07f568da6caf1f2d7aea7ef212ee7972aa8ed936
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-01-30 14:07:20 -0800

    Add hashing for genes and associated phenotypes

--- main.py
+++ main.py
@@ -3,15 +3,6 @@ import matplotlib.pyplot as plt
 import json
 from pymongo import MongoClient
 
-#Todo
-# list all keys
-# account for end range of degrees
-#is int? degrees
-# successfully found # of degrees from start
-#if first query fails return error.
-# change exit to restart while loop
-#integrate assert
-#graceful shutdown collection drop
 
 def mongo_init(MongoDBclient):
     MongoDBclient=MongoClient('localhost', 27017)
@@ -19,55 +10,62 @@ def mongo_init(MongoDBclient):
     return db
 
 def main():
+    #Initialize MongoDB and a mongo collection.
     mongoDBclient=None
     db=mongo_init(mongoDBclient)
-    dbcollection=db['CancerRelatedGenes']
+    dbcollection=db['CancersAndRelatedGenes']
 
+    #Read cancers and associated genes information from json file into MongoDB collection.
     with open('cancergenes.json') as file:
         fileData=json.load(file)
 
     dbcollection.insert_many(fileData)
     
-    query=None
-    start=None
-    result=None
-    degreeList=[]
-    diseaseNetwork = nx.Graph()
-    nodePair=[]
+    #Create and fill dictionary with values of genes and associated cancers.
+    cancerGenesDict={}
+    with open('cancergenes.json') as file2:
+        fileData2=json.load(file2)
+        for object in fileData2:
+            for key,values in object.items():
+                cancerGenesDict[key]=values
+                for value in values:
+                    if value not in cancerGenesDict:
+                        cancerGenesDict[value]=[key]
+                    else:
+                        cancerGenesDict[value].append(key)
 
-    while start==None:
-        print("Please enter 'gene' or 'phenotype' to begin.")
-        start=str(input())
+    #Enter gene or phenotype of interest and how many degrees of connections
+    #from starting point to display in graph.
+    querySet=set()
+    genePhenotypeNetwork = nx.Graph()
+    while True:
+        print("Please enter your gene of phenotype of interest to begin.")
+        initialQuery=str(input())
 
-        if start!="gene" and start!="phenotype":
-            print("Unrecognized input.")
-            start=None
-    
-    if start=="gene":
-        print("Please enter the gene of interest.")
-        query=str(input())
-        result=dbcollection.find({"Associated Gene" : query}, {'_id': False})
-    else:
-        print("Please enter the phenotype of interest.")
-        query=str(input())
-        result=dbcollection.find({"Classification" : query}, {'_id': False})
+        if initialQuery in cancerGenesDict:
+            print(cancerGenesDict[initialQuery])
+            querySet.add(initialQuery)
+            break
+        else:
+            print("Query not Found.")
 
     print("Please enter the number of degrees to display.")
     degrees=int(input())
-    for document in result:
-        for key,value in document.items():
-            # print("key=", key)
-            print("value =", value)
-            nodePair.append(value)
-        print("nodePair =", nodePair)
-        diseaseNetwork.add_edge(nodePair[0],nodePair[1])
-        degreeList.append(nodePair[0])
-        nodePair=[]
 
-    pos=nx.spring_layout(diseaseNetwork,k=3)
+    #Add nodes  and edge to graph based on initial interest gene or phenotype 
+    #and degrees of separation. 
+    for _ in range(degrees):
+        tempSet=set()
+        for query in querySet:
+            for value in cancerGenesDict[query]:
+                genePhenotypeNetwork.add_edge(query,value)
+                tempSet.add(value) 
+        querySet=tempSet
+
+    pos=nx.spring_layout(genePhenotypeNetwork,k=3)
     plt.figure(3,figsize=(16,16))
-    nx.draw_networkx(diseaseNetwork, pos = pos, with_labels = True)
-    plt.savefig("diseaseNetwork.png")
+    nx.draw_networkx(genePhenotypeNetwork, pos = pos, with_labels = True)
+    plt.savefig("genePhenotypeNetwork.png")
     dbcollection.drop()
 
 if __name__=="__main__":
