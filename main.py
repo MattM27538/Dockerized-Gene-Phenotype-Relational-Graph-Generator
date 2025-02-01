8e65e5e323bc30bfa11997059c8f8f99fb63315a
Matthew Martinez <mmartinez18@mail.csuchico.edu>
2025-01-31 20:03:31 -0800

    remove mongo and dependency list for docker

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import json
-from pymongo import MongoClient
-
-
-def mongo_init(MongoDBclient):
-    MongoDBclient=MongoClient('localhost', 27017)
-    db=MongoDBclient['GenePhenotypeDB']
-    return db
 
 def draw_graph(genePhenotypeNetwork):
     pos=nx.spring_layout(genePhenotypeNetwork,k=3)
@@ -20,29 +13,28 @@ def draw_graph(genePhenotypeNetwork):
 
 # Read in diseases and associated gene(s) information from json file and 
 # store information in dictionary.
-def read_json_file(cancerGenesDict):
+def read_json_file(genePhenotypeDict):
     with open('cancergenes.json') as jsonFile:
         jsonFileData=json.load(jsonFile)
     
     for object in jsonFileData:
         for key,values in object.items():
-            cancerGenesDict[key]=values
+            genePhenotypeDict[key]=values
             for value in values:
-                if value not in cancerGenesDict:
-                    cancerGenesDict[value]=[key]
+                if value not in genePhenotypeDict:
+                    genePhenotypeDict[value]=[key]
                 else:
-                    cancerGenesDict[value].append(key)
+                    genePhenotypeDict[value].append(key)
 
     jsonFile.close()
 
 #Enter gene or phenotype of interest and how many degrees of connections
 #from starting point to display in graph.
-def get_user_input(cancerGenesDict,querySet):
+def get_user_input(genePhenotypeDict,querySet):
     while True:
         print("Please enter your gene or phenotype of interest to begin.")
         initialQuery=str(input())
-        if initialQuery in cancerGenesDict:
-            print(cancerGenesDict[initialQuery])
+        if initialQuery in genePhenotypeDict:
             querySet.add(initialQuery)
             break
         else:
@@ -54,35 +46,26 @@ def get_user_input(cancerGenesDict,querySet):
 
 #Add nodes and edge to graph based on initial interest gene or phenotype 
 #and degrees of separation. 
-def add_nodes_to_graph(degrees,querySet,cancerGenesDict,genePhenotypeNetwork):
+def add_nodes_to_graph(degrees,querySet,genePhenotypeDict,genePhenotypeNetwork):
     for _ in range(degrees):
         tempSet=set()
         for query in querySet:
-            for value in cancerGenesDict[query]:
+            for value in genePhenotypeDict[query]:
                 genePhenotypeNetwork.add_edge(query,value)
                 tempSet.add(value) 
         querySet=tempSet
 
 def main():
-    #Initialize MongoDB and a mongo collection.
-    mongoDBclient=None
-    db=mongo_init(mongoDBclient)
-    dbcollection=db['Gene-Phenotype-Graph-Collection']
-    
-    #Create and fill dictionary with values of genes and associated cancers.
-    cancerGenesDict={}
-    read_json_file(cancerGenesDict)
+    #Create and fill dictionary with values of genes and associated disease.
+    genePhenotypeDict={}
+    read_json_file(genePhenotypeDict)
 
     querySet=set()
     genePhenotypeNetwork = nx.Graph()
-    degrees=get_user_input(cancerGenesDict,querySet)
+    degrees=get_user_input(genePhenotypeDict,querySet)
 
-    add_nodes_to_graph(degrees,querySet,cancerGenesDict,genePhenotypeNetwork)
+    add_nodes_to_graph(degrees,querySet,genePhenotypeDict,genePhenotypeNetwork)
     
     draw_graph(genePhenotypeNetwork)
-    
-    #Remove after testing.
-    dbcollection.drop()
-
 if __name__=="__main__":
     main()
